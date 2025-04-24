a120045928c2e98e91f4f515f5b582ec32b92786
Sugumar S <sugumar2210801@ssn.edu.in>
2025-04-24 23:34:12 +0530

    Update settings.py

--- stock_prediction/settings.py
+++ stock_prediction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a8p+a1svq-7ms$_sft=sjtqwr%553xqu%miw7u%=0+2c$1vq6j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =["*"]
 
